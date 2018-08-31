b096d8370dfd85481be10ed369c8bf9cb6cd982b
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-31 09:08:53 +0700

    create user class

---
+++ src/main/java/com/codegym/blog/response/User.java
@@ -0,0 +1,17 @@
+package com.codegym.blog.response;
+
+public class User {
+
+    private String username;
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
